41bd958bbcc5c8ee5f2e959f1bd890352da35fa1
schmittov <mariomitov31@gmail.com>
2023-12-28 17:39:39 +0100

    Created methodes and functions for login and registration

---
+++ InternshipChat/Chat.Domain/MethodesAndFunctions/MenuOptions.cs
@@ -0,0 +1,32 @@
+﻿namespace Chat.Domain.MethodesAndFunctions
+{
+    public class MenuOptions
+    {
+        public static Dictionary<int, string> SignInMenuOptions = new()
+        {
+            { 1, "Logiraj se" },
+            { 2, "Registriraj se" },
+        };
+        public static Dictionary<int, string> MainMenuOptions = new()
+        {
+            { 1, "Grupni kanali" },
+            { 2, "Privatne poruke" },
+            { 3, "User management" },
+            { 4, "Postavke profila" },
+            { 5, "Odjava iz profila" },
+        };
+        public static Dictionary<int, string> GroupChatMenuOptions = new()
+        {
+            { 1, "Kreiranje novog kanala" },
+            { 2, "Ulazak u kanal" },
+            { 3, "Ispis svih kanala u koje je korisnik ušao" },
+            { 4, "Vrati se nazad" },
+        };
+        public static Dictionary<int, string> PrivateChatMenuOptions = new()
+        {
+            { 1, "Nova poruka" },
+            { 2, "Ispis svih korisnika" },
+            { 3, "Vrati se nazad" },
+        };
+    }
+}
