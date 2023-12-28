23dc3dbfdf3493455ac2e9c3f23bd6c85f9daeb1
schmittov <mariomitov31@gmail.com>
2023-12-28 17:41:54 +0100

    Created basic output

---
+++ InternshipChat/Chat.Presentation/Program.cs
@@ -0,0 +1,22 @@
+﻿using Chat.Domain.MethodesAndFunctions;
+Console.CursorVisible = false;
+
+Navigation.MainNavigation(MenuOptions.SignInMenuOptions);
+Console.Clear();
+int secondMenu = Navigation.MainNavigation(MenuOptions.MainMenuOptions);
+Console.Clear();
+switch (secondMenu)
+{
+    case 1:
+        Navigation.MainNavigation(MenuOptions.GroupChatMenuOptions);
+        break;
+    case 2:
+        Navigation.MainNavigation(MenuOptions.PrivateChatMenuOptions);
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+}
