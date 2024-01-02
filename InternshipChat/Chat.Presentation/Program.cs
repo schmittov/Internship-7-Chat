39c863259734cc2f28808d94700d3937e4e95d98
schmittov <mariomitov31@gmail.com>
2024-01-02 21:12:16 +0100

    Updated output

--- InternshipChat/Chat.Presentation/Program.cs
+++ InternshipChat/Chat.Presentation/Program.cs
@@ -1,22 +1,36 @@
 ﻿using Chat.Domain.MethodesAndFunctions;
 Console.CursorVisible = false;
+int secondMenu=0, thirdMenu=0;
 
-Navigation.MainNavigation(MenuOptions.SignInMenuOptions);
-Console.Clear();
-int secondMenu = Navigation.MainNavigation(MenuOptions.MainMenuOptions);
-Console.Clear();
-switch (secondMenu)
+while (true)
 {
-    case 1:
-        Navigation.MainNavigation(MenuOptions.GroupChatMenuOptions);
-        break;
-    case 2:
-        Navigation.MainNavigation(MenuOptions.PrivateChatMenuOptions);
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-    case 5:
-        break;
-}
+    Navigation.MainNavigation(MenuOptions.SignInMenuOptions);
+    //check login or registration
+    Console.Clear();
+    while (true)
+    {
+        secondMenu = Navigation.MainNavigation(MenuOptions.MainMenuOptions);
+        
+        Console.Clear();
+        if (secondMenu == 0)
+            break;
+
+        switch (secondMenu)
+        {
+            case 1:
+                thirdMenu = Navigation.MainNavigation(MenuOptions.GroupChatMenuOptions);
+                break;
+            case 2:
+                thirdMenu = Navigation.MainNavigation(MenuOptions.PrivateChatMenuOptions);
+                break;
+            case 3:
+                thirdMenu = Navigation.MainNavigation(MenuOptions.UserManagmentMenuOptions);
+                break;
+            case 4:
+                thirdMenu = Navigation.MainNavigation(MenuOptions.ProfileSettingsMenuOptions);
+                break;
+        }
+        Console.Clear();
+        Environment.Exit(0); //do action
+    }
+}    
