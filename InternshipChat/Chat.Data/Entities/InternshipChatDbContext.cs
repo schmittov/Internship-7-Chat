910c0b203ec681b03f3b6da00bd41674ee8d9ff4
schmittov <mariomitov31@gmail.com>
2024-01-03 23:46:07 +0100

    Created Database context

---
+++ InternshipChat/Chat.Data/Entities/InternshipChatDbContext.cs
@@ -0,0 +1,73 @@
+﻿using Chat.Data.Entities.Models;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Chat.Data.Seeds;
+
+namespace Chat.Data.Entities
+{
+    public class InternshipChatDbContext : DbContext
+    {
+        public InternshipChatDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<GroupChat> GroupChats => Set<GroupChat>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<UserGroup> UserGroups => Set<UserGroup>();
+        public DbSet<GroupMessage> GroupMessages => Set<GroupMessage>();
+        public DbSet<PrivateMessage> PrivateMessages => Set<PrivateMessage>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserGroup>()
+            .HasKey(ug => new { ug.UserId, ug.GroupChatId });
+
+            modelBuilder.Entity<UserGroup>()
+                .HasOne(ug => ug.User)
+                .WithMany(u => u.UserGroups)
+                .HasForeignKey(ug => ug.UserId);
+
+            modelBuilder.Entity<UserGroup>()
+                .HasOne(ug => ug.GroupChat)
+                .WithMany(gc => gc.UserGroups)
+                .HasForeignKey(ug => ug.GroupChatId);
+
+            modelBuilder.Entity<PrivateMessage>()
+                .HasOne(pm => pm.User)
+                .WithMany(u => u.PrivateMessages)
+                .HasForeignKey(pm => pm.IdSender);
+
+            modelBuilder.Entity<GroupMessage>()
+                .HasOne(gm => gm.GroupChat)
+                .WithMany(gc => gc.GroupMessages)
+                .HasForeignKey(gm => gm.GroupChatId);
+
+            DatabaseSeeder.Seed(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+
+    public class InternshipChatDbContextFactory : IDesignTimeDbContextFactory<InternshipChatDbContext>
+    {
+        public InternshipChatDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddXmlFile("Chat.config")
+                .Build();
+
+            config.Providers
+                .First()
+                .TryGet("connectionStrings:add:InternshipChat:connectionString", out var connectionString);
+
+            var options = new DbContextOptionsBuilder<InternshipChatDbContext>()
+                .UseNpgsql(connectionString)
+                .Options;
+
+            return new InternshipChatDbContext(options);
+        }
+    }
+}
