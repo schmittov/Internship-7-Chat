a2d3ce68e3d54011e4cc671d2ca7a2f8fa04f7c9
schmittov <mariomitov31@gmail.com>
2024-01-04 00:23:37 +0100

    Created factories

---
+++ InternshipChat/Chat.Domain/Factories/DbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Chat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+namespace Chat.Domain.Factories
+{
+    public static class DbContextFactory
+    {
+        public static InternshipChatDbContext GetInternshipChatDbContext()
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseNpgsql(ConfigurationManager.ConnectionStrings["InternshipChat"].ConnectionString)
+                .Options;
+
+            return new InternshipChatDbContext(options);
+        }
+    }
+}
---
+++ InternshipChat/Chat.Domain/Factories/RepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using Chat.Domain.Repositories;
+
+namespace Chat.Domain.Factories
+{
+    public class RepositoryFactory
+    {
+        public static TRepository Create<TRepository>()
+            where TRepository : BaseRepository
+        {
+            var dbContext = DbContextFactory.GetInternshipChatDbContext();
+            var repositoryInstance = Activator.CreateInstance(typeof(TRepository), dbContext) as TRepository;
+
+            return repositoryInstance!;
+        }
+    }
+}
